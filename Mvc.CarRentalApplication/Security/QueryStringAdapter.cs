8b0132c4f99abd2755c1b02c1f82c85c16f62dc2
rennishj <rennishj@gmail.com>
2014-02-21 10:06:32 -0500

    Added the query string obfuscation class

---
+++ Mvc.CarRentalApplication/Security/QueryStringAdapter.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Mvc.CarRentalApplication
+{
+    public class QueryStringAdapter
+    {
+
+        public string ObfuscateQueryString(string input)
+        {
+
+            return string.Empty;
+        }
+    }
+}
