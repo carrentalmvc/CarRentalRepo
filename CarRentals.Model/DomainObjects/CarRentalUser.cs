b9ea32027c7fc1eb0532ba869fe3ebb94f8eaf3c
rennishj <rennishj@gmail.com>
2014-02-21 23:32:53 -0500

    Adding the CarRentalUser class

---
+++ CarRentals.Model/DomainObjects/CarRentalUser.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace CarRentals.Model
+{
+  public class CarRentalUser
+    {
+        public int UserId { get; set; }
+
+        [Display(Name = "Email Address")]
+        [Required]
+        public string EmailAddress { get; set; }       
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        [DataType(DataType.Password)]
+        [Required]
+        public string Password { get; set; }
+
+        [Display(Name = "Confirm Password")]
+        [Compare("Password")]
+        [Required]
+        public string ConfirmPassword { get; set; }
+    }
+  
+}
