d88f4359587549edf20c484dce261555e9690fd0
rennishj <rennishj@gmail.com>
2014-02-27 20:31:51 -0500

    Added script for inserting car rental roles

---
+++ CarRental.ConsoleTestApp/DbScripts/CarRental_Roles.sql
@@ -0,0 +1,22 @@
+﻿INSERT INTO [CarRental].[dbo].[CarRentalRole]
+           ([Name]
+           ,[IsActive]
+           ,[IsDeleted]
+           ,[CreatedDtm]
+           ,[UpdatedDtm])
+     VALUES
+           (
+              'Admin',
+              1,0,GETDATE(),null
+           
+           ),
+           (
+              'Guest User',
+              1,0,GETDATE(),null
+           
+           ),
+           (
+              'Normal User',
+              1,0,GETDATE(),null
+           
+           )
