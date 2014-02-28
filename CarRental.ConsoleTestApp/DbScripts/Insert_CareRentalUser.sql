f1104bded0066b9c1e05f4ae5faccaf1540902b7
rennishj <rennishj@gmail.com>
2014-02-27 19:49:03 -0500

    Added the Insert_CarRental_User stored procedure

---
+++ CarRental.ConsoleTestApp/DbScripts/Insert_CareRentalUser.sql
@@ -0,0 +1,42 @@
+﻿USE CarRental
+GO
+
+Create PROCEDURE dbo.CarRental_InsertUser (
+	 @UserId INT OUTPUT
+	,@RoleId INT
+	,@FirstName VARCHAR(50)
+	,@LastName VARCHAR(50)
+	,@EmailAddress VARCHAR(25)
+	,@Password VARBINARY(500)
+	,@IsActive BIT
+	)
+AS
+BEGIN
+	
+	
+	
+	INSERT INTO [CarRental].[dbo].[CarRentalUser]
+           ([RoleId]
+           ,[EmailAddress]
+           ,[FirstName]
+           ,[LastName]
+           ,[Password]
+           ,[CreatedDtm]
+           ,[UpdateDtm]
+           ,[IsActive])
+     VALUES
+           (
+               @RoleId,
+               @EmailAddress,
+               @FirstName,
+               @LastName,
+               @Password,
+               GETDATE(),
+               null,
+               1           
+           )
+           
+           set @UserId = SCOPE_IDENTITY();
+           
+           select @UserId
+END
